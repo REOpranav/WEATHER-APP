dc163e7f2e3aed22ebd223a90c20165c36650834
REOpranav <pranavavadivel01@gmail.com>
2023-07-29 23:02:43 +0530

    Add new components

---
+++ src/Image.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Image = () => {
+  return (
+    <div>
+         <img src="https://o.remove.bg/downloads/4e49dbc5-1519-4ec0-a4ad-eddb32928d69/png-transparent-planet-earth-earth-icon-earth-globe-world-sphere-thumbnail-removebg-preview.png" 
+             className='image' />
+    </div>
+  )
+}
+
+export default Image
