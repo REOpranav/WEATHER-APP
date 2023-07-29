06a1bd8fd857f73a88f58fa2e1eed80a32298e63
REOpranav <pranavavadivel01@gmail.com>
2023-07-29 18:36:40 +0530

    Updating and Creating functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react'
+import WeatherNotify from './WeatherNotify';
+import Input from './Input';
+
+
 
 function App() {
+  const key = 'e7a5e6824ab7d08351501ac5e7ba440b'  ;
+
+  const [city,setCity] = useState('')
+
+  const [outlet,setOutlet]=useState('')
+  
+  const fetchItem = async()=>{
+     try {
+        const responce = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`)
+        setOutlet(responce.data)
+        console.log(responce.data)
+
+      } catch (err) {
+        alert("Plse Enter Proper City Name")
+     }
+  }
+
+  const handleSubmit = (e)=>{ 
+    e.preventDefault()
+    setCity('')
+} 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+     
+      <Input 
+        city={city}
+        setCity={setCity}
+        handleSubmit={handleSubmit}
+        outlet={outlet}
+        fetchItem={fetchItem}
+      />
+     
+     <WeatherNotify 
+        outlet={outlet}
+      />
+
+     </div>
+  
+  
   );
 }
 
