a7195bda54cb8dd44fd143645b5cbfbc141b20f5
REOpranav <pranavavadivel01@gmail.com>
2023-07-30 10:56:03 +0530

    change the image

--- src/Image.js
+++ src/Image.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Image = () => {
   return (
     <div>
-         <img src="https://o.remove.bg/downloads/4e49dbc5-1519-4ec0-a4ad-eddb32928d69/png-transparent-planet-earth-earth-icon-earth-globe-world-sphere-thumbnail-removebg-preview.png" 
+         <img src="https://www.pngmart.com/files/22/Earth-Drawing-PNG-HD.png" 
              className='image' />
     </div>
   )
