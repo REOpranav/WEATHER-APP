d2024308200f2f729710d5333f5aea0d16058513
pranav <pranavavadivel01@gmail.com>
2024-03-20 22:46:37 +0530

    Add antd

--- src/App.js
+++ src/App.js
@@ -58,9 +58,9 @@ function App() {
     setCity("");
   };
   
-  if (cbStatus !== "ACTIVE") {
-    return <p>loading...</p>
-  }
+  // if (cbStatus !== "ACTIVE") {
+  //   return <p>loading...</p>
+  // }
 
   
   return (
